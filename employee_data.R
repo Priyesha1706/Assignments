07349ab199cd7d7af764627a78d43322f7becc94
Priyesha <priyeshashrivastava@gmail.com>
2020-03-27 15:57:27 +0530

    Update employee_data.R
    updted_employee_data_lmr

--- employee_data.R
+++ employee_data.R
@@ -1,7 +1,35 @@
 View(emp_data)
 attach(emp_data)
-cor(Salary_hike,Churn_out_rate)
+# 1st Moment Business Decision Model
+summary(emp_data)
+# 2nd Moment Business Decision Model
+var(Salary_hike)
+sd(Salary_hike)
+install.packages("e1071")
+library(e1071)
+# 3rd Moment Business Decision Model
+skewness(Salary_hike)
+# 4th Moment Business Decision Model
+kurtosis(Salary_hike)
+# Basics Plotting (BoxPlot & BarPlot)
+boxplot(Salary_hike)
+barplot(Salary_hike)
+# Plotting a Histogram 
+hist(Salary_hike)
+# Checking the Normality or Linearity of distribution
+qqnorm(Salary_hike)
+# Plotting a scatter plot of Emp Dataset
+plot(emp_data)
+# Determining the Correlation of Emp Dataset
+cor(emp_data)
+# Preparing a Simple Linear Regression model 
 emp_data_model <- lm(Churn_out_rate~Salary_hike)
 summary(emp_data_model)
-plot(emp_data_model)
-#Hence the P-value is less than 0.05. So X varibale is significance and also Multiple R-Square value is 0.8312 That's mean this model will predict the output 83.12% time correct
+# Transformation of above model
+emp_data_model_log <- lm(Churn_out_rate~log(Salary_hike))
+summary(emp_data_model_log)
+# Final predicted Simple Linear Regression model
+FinalModel <- lm(log(Churn_out_rate) ~ log(Salary_hike))
+summary(FinalModel)
+plot(FinalModel)
+#The Final Model is Predicted with an accuracy of 88.91 %.
