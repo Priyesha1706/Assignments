18343baf1bc1a887a9233dded296e171f8df4fa2
Priyesha <priyeshashrivastava@gmail.com>
2020-04-03 20:08:13 +0530

    Update credit.R
    credit_logistic regression

--- credit.R
+++ credit.R
@@ -1,36 +1,34 @@
 View(creditcard)
 attach(creditcard)
+creditcard <- creditcard[,-1]
 library(caret)
 dmy <- dummyVars("~ .", data = creditcard, fullRank = T)
-credit_data <- data.frame(predict(dmy, newdata = creditcard))
-str(credit_data)
-attach(credit_data)
-summary(credit_data)
+creditcard <- data.frame(predict(dmy, newdata = creditcard))
+str(creditcard)
+attach(creditcard)
+summary(creditcard)
 sd(age)
 sd(income)
 sd(expenditure)
 var(age)
 var(income)
 var(expenditure)
+library(e1071)
 skewness(age)
 skewness(income)
 skewness(expenditure)
 kurtosis(age)
 kurtosis(income)
 kurtosis(expenditure)
-plot(credit_data)
-hist(card,age)
-hist(card,income)
-hist(card,expenditure)
+plot(creditcard)
 credit_model <- lm(card.yes ~.,data = creditcard)
 summary(credit_model)
 plot(credit_model)
 install.packages("aod")
-logit <- glm(card.yes ~ .,data = creditcard,family = "binomial")
+logit <- glm(card.yes ~ reports + age + income + share + expenditure + factor(owner.yes)+ factor(selfemp.yes)+ dependents + months + majorcards + active ,data = creditcard,family = "binomial")
 summary(logit)
 plot(logit)
 library(lattice)
-
 #odd ratio
 exp(coef(logit))
 
@@ -40,12 +38,12 @@ prob <- predict(logit,credit_data,type="response")
 # We are going to use NULL and Residual Deviance to compare the between different models
 
 # Confusion matrix and considering the threshold value as 0.5 
-confusion<-table(prob>0.5,creditcard$card)
+confusion<-table(prob>0.5,creditcard$card.yes)
 confusion
 # Model Accuracy 
 Accuracy<-sum(diag(confusion)/sum(confusion))
 Accuracy 
 #roc curve
 library(pROC)
-roccurve <-roc(credit_data$card.yes ~prob)
+roccurve <-roc(creditcard$card.yes ~prob)
 plot(roccurve)
