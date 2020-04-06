c4ae4fb7f6339e91b660e66af1f24a303600133d
Priyesha <priyeshashrivastava@gmail.com>
2020-04-06 19:40:21 +0530

    Create airlines.R
    airlines_dataset_clustering_kmeans

---
+++ airlines.R
@@ -0,0 +1,31 @@
+View(EastWestAirlines)
+colnames(EastWestAirlines)
+Eastwest_Airlines <- EastWestAirlines[,2:12] 
+normalize_airline <- scale(Eastwest_Airlines)
+View(normalize_airline)
+ d <- dist(normalize_airline, method = "euclidean")# distance matrix
+ hcluster <- hclust(d, method = "complete")
+ plot(hcluster, hang = -1)
+ #h1cluster <- hclust(d,method ="average")
+ #plot(h1cluster, hang = -1)
+ #h2cluster <- hclust(d,method = "centroid")
+ #plot(h2cluster, hang = -1)
+ #h3cluster <- hclust(d,method = "single")
+ #plot(h3cluster, hang = -1)
+ group_airlines <- cutree(hcluster, k = 5)
+rect.hclust(hcluster,k =5,plot(hcluster, hang = -1),border = "red") 
+new_EastWestAirlines <- cbind(EastWestAirlines,group_airlines)
+setNames(EastWestAirlines_New,'group_airlines')
+aggregate(EastWestAirlines[,2:12], by=list(EastWestAirlines_New$ group_airlines), FUN = mean)
+# K-MEANS CLustering
+airline_Kmean <- kmeans(normalize_airline,5)
+str(airline_Kmean)
+airline_Kmean$centers
+new_EastWestAirlines <- cbind(new_EastWestAirlines,airline_Kmean$cluster)
+colnames(new_EastWestAirlines)
+aggregate(EastWestAirlines[,2:12],by=list(new_EastWestAirlines$`airline_Kmean$cluster`),FUN = mean)
+install.packages("animation")
+library(animation)
+windows()
+airline_kmean <- kmeans.ani(normalize_airline,5)
+
